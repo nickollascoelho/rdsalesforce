97260609e55527597f27341876455825dcea62b3
Nickollas Coelho <nickollas.coelho@gmail.com>
2014-11-27 00:35:58 -0200

    Person change

--- lib/rdsalesforce/person.rb
+++ lib/rdsalesforce/person.rb
@@ -1,12 +1,16 @@
 module RDSalesForce
   class Person
-		#:name, :last_name, :email, :company, :job_title, :phone, :website,
-    attr_accessor  :client, :attributes
+
+    attr_accessor  :client, :name, :last_name, :email, :company, :job_title, :phone, :website
 
     def initialize(client, parameters={})
         raise ArgumentError,"client class must be RDSalesForce::Client" unless client.class == RDSalesForce::Client
         self.client = client
-        self.attributes = parameters
+        parameters.each do |key, val| 
+          if self.respond_to? key
+            send("#{k}=", val)
+          end 
+        end
     end
     
     def save_as_salesforce_contact

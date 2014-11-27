ae4a2bd34d5579e27ca594d7d60c51bc16ce601b
Nickollas Coelho <nickollas.coelho@gmail.com>
2014-11-27 02:01:43 -0200

    Auth

--- lib/rdsalesforce/client.rb
+++ lib/rdsalesforce/client.rb
@@ -3,12 +3,14 @@ module RDSalesForce
   class Client
     require 'rdsalesforce/person'
     
-    attr_accessor :host, :username, :password
+    attr_accessor :username, :password, :security_token, :client_id, :client_secret
 
     def initialize(host, username, password)
-      self.host = host
-      self.username = username
-      self.password = password
+      self.username = 'nickollascoelho@gmail.com'
+      self.password = 'salesforce123'
+      self.security_token => 'aL2CXA3jAu7H6jPoZIfFG1rNr',
+      self.client_id      => '3MVG9fMtCkV6eLhfcGInOg5ML9H0SOqpNbd53dLHJsX62lWDfdPhZJ.L5KqDwLFABYRwif2pG7ppdNxZEsgVd',
+      self.client_secret  => '849254060868716892'
     end
 
     def create_person(parameters=nil)
@@ -18,7 +20,9 @@ module RDSalesForce
     def salesforce_api
       @salesforce = Restforce.new :username => self.username,
         :password => self.password,
-        :host => self.host
+        :security_token => self.security_token, 
+        :client_id => self.client_id, 
+        :client_secret => self.client_secret
       @salesforce.authenticate!
       return @salesforce;
     end
